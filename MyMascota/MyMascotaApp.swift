6ed130896ae43a9c0281f652b6169ac874309614
Daniel Beltran <danielesbe9526@hotmail.com>
2025-05-08 10:42:27 -0500

    initial commit

--- MyMascota/MyMascotaApp.swift
+++ MyMascota/MyMascotaApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct MyMascotaApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Pet.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
